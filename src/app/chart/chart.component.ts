37f3221e7b50eb8d0adf1a5e84d13968c0987292
FrancoHasper95 <francoalfredo2014@gmail.com>
2020-12-31 11:28:58 -0300

    cambios en la grafica base

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -14,15 +14,61 @@ export class ChartComponent {
   oneToOneFlag = true;
 
   chartOptions: Options = {
+    chart: {
+      type: "spline"
+    },
+    title: {
+      text: "Historial de Precios Producto"
+    },
+    subtitle: {
+      text: "Aqui un resultado ej Pan"
+    },
+    xAxis: {
+      categories: ["aqui", "el", "for", "de", "las", "fechas",
+        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    },
+    yAxis: {
+      title: {
+        text: "Precio $"
+      }
+    },
+    tooltip: {
+      valueSuffix: " $"
+    },
+    //Seccion datos.
     series: [
       {
+        name: 'Producto 1',
+        type: "line",
+        data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+      },
+      {
+        name: 'producto 2',
         type: "line",
         data: [1, 2, 3]
       }
     ]
   };
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*import { Component, OnInit } from '@angular/core';
 
